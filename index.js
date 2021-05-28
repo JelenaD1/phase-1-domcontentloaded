1c84d796b744aed229e36ffb7ebbe4e81167f924
Jelena <jelena.durovic@powerhrg.com>
2021-05-28 09:11:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const event = document.querySelector("#text")
+   event.textContent = "This is really cool!"
+  }); 
